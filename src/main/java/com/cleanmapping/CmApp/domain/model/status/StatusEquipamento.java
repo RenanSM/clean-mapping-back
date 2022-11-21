8f306d20bb160dd8abe00465e8ea2d9512270de9
devsys-b <devsys-b@DESKTOP-H3FI62G>
2022-11-21 09:55:54 -0300

    Criação do Model

---
+++ src/main/java/com/cleanmapping/CmApp/domain/model/status/StatusEquipamento.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cleanmapping.CmApp.domain.model.status;
+
+/**
+ *
+ * @author devsys-b
+ */
+public enum StatusEquipamento {
+    
+}
