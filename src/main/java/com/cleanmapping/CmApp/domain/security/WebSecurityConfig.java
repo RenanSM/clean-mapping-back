a53674cef3ea24c5685045e9d04c7741bee2f3b6
Devsys-B <Devsys-B@DESKTOP-7HSJM51>
2022-11-22 11:13:28 -0300

    Configurando (Auth,Cript)

--- src/main/java/com/cleanmapping/CmApp/domain/security/WebSecurityConfig.java
+++ src/main/java/com/cleanmapping/CmApp/domain/security/WebSecurityConfig.java
@@ -4,10 +4,14 @@
  */
 package com.cleanmapping.CmApp.domain.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 
@@ -25,4 +29,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                
     }
     
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new MessageDigestPasswordEncoder("MD5");
+    }
+    
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        
+        auth.inMemoryAuthentication()
+            .passwordEncoder(new MessageDigestPasswordEncoder("MD5"))
+                .withUser("renan")
+                .password(passwordEncoder().encode("123"))
+                .roles("ADMIN")
+            .and()
+                .withUser("reader")
+                .password(passwordEncoder().encode("reader1"))
+                .roles("USER")
+        ;
+    }
 }
